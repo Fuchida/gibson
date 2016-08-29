03dd90035b318df50f512b1bf43f7d401d60a07c
fuchida <abdlsalim@gmail.com>
2016-08-28 20:16:23 -0500

    Add gunicorn support

--- app/app.py
+++ app/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from werkzeug.contrib.fixers import ProxyFix
 
 from config import DEBUG_STATUS, HOST, PORT
 
 
 # Initilize Flask App
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
 from views import *
 
 if __name__ == "__main__":
