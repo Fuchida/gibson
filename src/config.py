a6dc70b2ea6cc1a14be97e58690baa9e805c4f75
fuchida <abdlsalim@gmail.com>
2016-08-31 20:32:15 -0500

    Allow local config for Repo and Secrete

--- src/config.py
+++ src/config.py
@@ -5,11 +5,14 @@ import os
 # reStructuredText not accepted for now
 ACCEPTED_FILE_FORMATS = "^.*\.md$"  # .md
 
+APP_POSTS_REPO_URL = None
+APP_REPO_SECRETE = None
+
 # Git Repo Config
 # Fetched via environmental variables
-GIT_REPO_URL = os.environ.get('POSTS_GIT_REPO')
+GIT_REPO_URL = os.environ.get('POSTS_GIT_REPO', APP_POSTS_REPO_URL)
 GIT_REPO_DIR_NAME = GIT_REPO_URL.split('/')[-1].replace('.git', '')
-GIT_REPO_SECRET = os.environ.get('POSTS_GIT_REPO_SECRET')
+GIT_REPO_SECRET = os.environ.get('POSTS_GIT_REPO_SECRET', APP_REPO_SECRETE)
 
 # Server Config
 PORT = 5000
