d96d96bcd71066f642a2e44fe02368edd65a8890
fuchida <abdlsalim@gmail.com>
2016-08-31 21:11:24 -0500

    Save variable for local and environment config

--- src/config.py
+++ src/config.py
@@ -5,14 +5,14 @@ import os
 # reStructuredText not accepted for now
 ACCEPTED_FILE_FORMATS = "^.*\.md$"  # .md
 
-APP_POSTS_REPO_URL = None
-APP_REPO_SECRETE = None
+POSTS_GIT_REPO = None
+POSTS_GIT_REPO_SECRET = None
 
 # Git Repo Config
 # Fetched via environmental variables
-GIT_REPO_URL = os.environ.get('POSTS_GIT_REPO', APP_POSTS_REPO_URL)
+GIT_REPO_URL = os.environ.get('POSTS_GIT_REPO', POSTS_GIT_REPO)
 GIT_REPO_DIR_NAME = GIT_REPO_URL.split('/')[-1].replace('.git', '')
-GIT_REPO_SECRET = os.environ.get('POSTS_GIT_REPO_SECRET', APP_REPO_SECRETE)
+GIT_REPO_SECRET = os.environ.get('POSTS_GIT_REPO_SECRET', POSTS_GIT_REPO_SECRET)
 
 # Server Config
 PORT = 5000
